2d97dd7686a4f368f771a9ecea27f35da49bfa82
sridhar <sridharan4321@gmail.com>
2024-04-21 19:07:27 +0530

    added customers

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,6 +6,11 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
  
 
 export default async function Page({
@@ -18,7 +23,7 @@ export default async function Page({
   }) {
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
-    const totalPages=fetchInvoicesPages(query)
+    const totalPages=await fetchInvoicesPages(query)
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
